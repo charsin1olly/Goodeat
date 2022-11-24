badc484323fa070e5d6dd1c1c69ff2038f06a9ee
Charsin Chan <116936412+charsin1olly@users.noreply.github.com>
2022-11-24 08:52:10 +0800

    stimulus -更正oder-btn檔名修正controller找不到路徑問題

--- app/javascript/controllers/order-btn_controller.js
+++ app/javascript/controllers/order-btn_controller.js
@@ -1,7 +1,18 @@
 
 import { Controller } from 'stimulus'; 
 export default class extends Controller {
+
+  static targets = [ "order-one" ]
+
   connect() {
     console.log("hello from StimulusJS")
   }
+
+  order(){
+    console.log("ok")
+
+  }
+
+
 }
+
